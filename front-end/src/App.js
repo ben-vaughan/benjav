681dac70292b11a15f288d0e74c85b6ee036b3d8
Ben Vaughan <56156727+ben-vaughan@users.noreply.github.com>
2022-06-08 19:48:20 +0100

    Minor change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Collie!
+          Hello Brian!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        This is my personal site
+      </body>
     </div>
   );
 }
